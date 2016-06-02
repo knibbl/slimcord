b8820c670f2e7096f6aca41f92b84bf3307bb0c4
Philipp Seiler <p.seiler@linuxmail.org>
2016-06-02 22:41:40 +0200

    - tutorial (home directory)

--- home/urls.py
+++ home/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from . import views
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include("home.urls"), name="home"),
-    ]
+#    url(r'^admin/', admin.site.urls),
+#    url(r'^', include("home.urls"), name="home"),
+    url(r'^$', views.home, name = "home"),
+]
 
