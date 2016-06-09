f75d58967cbd920124c50d7afbbed31fd9aa1dd0
Philipp Seiler <p.seiler@linuxmail.org>
2016-06-09 21:37:33 +0200

    initial commit of user model

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Users(models.Model):
+    user_email = models.EmailField(max_length=254)
+    user_name = models.CharField(max_length=30, primary_key=True)
+    user_password = models.CharField(max_length=100)
+    user_birth = models.DateTimeField('date published')
+    user_sex = models.NullBooleanField()
+    user_info = models.TextField()
+
+# Create your models here.
